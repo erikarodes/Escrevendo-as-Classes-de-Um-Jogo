d3b74a32580dff2187e5903afacaccb79e80ab92
Erika rodes <erickasophya15@gmail.com>
2024-02-27 02:31:48 -0300

    Projeto criado

---
+++ desafios da Dio/Escrevendo as Classes de Um Jogo/scr/index.js
@@ -0,0 +1,46 @@
+// Entrada de dados:
+const input = require('readline-sync');
+
+//Classe:
+class Heroi {
+
+    ataque = "";
+
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+
+        if (this.tipo === "mago") {
+            this.ataque = "magia";
+
+        } else if (this.tipo === "guerreiro") {
+            this.ataque = "espada";
+
+        } else if (this.tipo === "monge") {
+            this.ataque = "artes marciais";
+
+        } else if (this.tipo === "ninja") {
+            this.ataque = "shuriken";
+            
+        } else {
+            this.tipo = `- ${this.tipo} - é um TIPO INVÁLIDO`;
+            this.ataque = "ATAQUE INVÁLIDO";
+        }
+
+        // Saída -> ataque
+        console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
+    }
+
+}
+
+let nome = input.question('Informe do heroi: ');
+let idade = input.questionInt('Informe a idade do heroi: ');
+let tipo = input.question('Informe o tipo heroi [mago, guerreiro, monge ou ninja]: ');
+
+let heroi = new Heroi(nome, idade, tipo.toLowerCase());
+
+heroi.atacar(); 
